73e7b02cd60526ea460cbca5ec3dcb023e2f4d22
Gnusson.net <admin@gnusson.net>
2021-03-29 14:33:28 +0200

    Initial commit

---
+++ Moonstocks/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Moonstocks.Stores;
+using Moonstocks.ViewModels;
+using Moonstocks.Views;
+using System.Windows;
+
+namespace Moonstocks
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            NavigationStore navigationStore = new NavigationStore();
+
+            navigationStore.CurrentViewModel = new SignInViewModel(navigationStore);
+
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel(navigationStore)
+            };
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+}
